73fbdfd52b4f696fcb6dc623c8396ebb0bf48bc5
spaww <the.spaww@gmail.com>
2017-03-07 12:12:44 -0300

    Build 20170307-1 Remoção de arquivos desnecessários

--- local/app/everyz/installEveryz.sh
+++ local/app/everyz/installEveryz.sh
@@ -6,8 +6,8 @@
 INSTALAR="N";
 AUTOR="the.spaww@gmail.com"; 
 TMP_DIR="/tmp/upgZabbix";
-VERSAO_INST="Beta_20170307_1";
-VERSAO_EZ="1.0-beta30";
+VERSAO_INST="Beta_20170307_2";
+VERSAO_EZ="1.0-beta31";
 UPDATEBD="S";
 BRANCH="master";
 NOME_PLUGIN="EVERYZ";
