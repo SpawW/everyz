4f3af4cd4992107182cd1d16f2d4c92c06ab029f
SpawW <the.spaww@gmail.com>
2017-09-19 17:22:13 -0300

    Version - "1.1.2";

--- local/app/views/everyz.zbxe-config.view.php
+++ local/app/views/everyz.zbxe-config.view.php
@@ -17,6 +17,8 @@
  * * You should have received a copy of the GNU General Public License
  * * along with this program; if not, write to the Free Software
  * * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Date     * Objective                                                         *
+ * 20170919 * Limit maps configuration for versions before 3.4 of Zabbix        *
  * */
 
 /* * ***************************************************************************
