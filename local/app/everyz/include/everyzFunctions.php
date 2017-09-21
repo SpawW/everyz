4a37b5ab1938a5fa4b0a185a10624d09d9bc5bb4
SpawW <the.spaww@gmail.com>
2017-09-20 23:44:12 -0300

    Version 1.1.3.20170920.243

--- local/app/everyz/include/everyzFunctions.php
+++ local/app/everyz/include/everyzFunctions.php
@@ -24,7 +24,7 @@
 define("ZE_VER", "3.0");
 define("EZ_TITLE", 'EveryZ - ');
 define("ZE_COPY", ", ZE " . ZE_VER);
-define("EVERYZVERSION", 1.12);
+define("EVERYZVERSION", "1.1.3");
 define("EVERYZBUILD", 9);
 if (file_exists("lockEverys.php")) {
     $VG_INSTALL = true;
